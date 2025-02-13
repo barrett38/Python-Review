6d902a6d5ddb95a338648f1d437b57101bc4e744
Barrett Anderson <barrett1614@gmail.com>
2025-02-13 10:44:32 -0600

    Finished generate_qrcode

---
+++ generate_qrcode.py
@@ -0,0 +1,16 @@
+# Install pip3 install qrcode Image
+import qrcode
+def generate_qrcode(text):
+    qr = qrcode.QRCode(
+        version=1,
+        error_correction=qrcode.constants.ERROR_CORRECT_L,
+        box_size=10,
+        border=4,
+    )
+    qr.add_data(text)
+    qr.make(fit=True)
+    img = qr.make_image(fill_color="black", back_color="white")
+    img.save("qrcode1.png")
+
+url = input("Enter URL: ")
+generate_qrcode(url)
